dafcd1b6bf29734e6593db072ef691f9aad256ca
alequerenghi <querenghialessandro@icloud.com>
2025-12-03 15:58:51 +0100

    testing different topologies

--- src/main/java/examples/SymbolicRegressionTest.java
+++ src/main/java/examples/SymbolicRegressionTest.java
@@ -2,17 +2,29 @@ package examples;
 
 import static io.jenetics.util.RandomRegistry.random;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
+import java.util.zip.GZIPInputStream;
+
+import com.sun.tools.javac.Main;
 
 import cellular.CellularEngine;
 import cellular.GraphMap;
+import cellular.GraphMaps;
+import io.jenetics.engine.Codec;
 import io.jenetics.engine.EvolutionResult;
 import io.jenetics.engine.EvolutionStatistics;
 import io.jenetics.engine.EvolutionStream;
-import io.jenetics.engine.Limits;
+import io.jenetics.ext.util.Tree;
 import io.jenetics.ext.util.TreeNode;
 import io.jenetics.prog.ProgramGene;
 import io.jenetics.prog.op.EphemeralConst;
@@ -24,115 +36,91 @@ import io.jenetics.prog.regression.Error;
 import io.jenetics.prog.regression.LossFunction;
 import io.jenetics.prog.regression.Regression;
 import io.jenetics.prog.regression.Sample;
-import io.jenetics.stat.DoubleMomentStatistics;
+import io.jenetics.stat.MinMax;
 import io.jenetics.util.ISeq;
 
 public class SymbolicRegressionTest {
 
-	private static final ISeq<Op<Double>> OPS = ISeq.of(MathOp.ADD, MathOp.SUB, MathOp.MUL);
-
-	private static final ISeq<Op<Double>> TMS = ISeq.of(Var.of("x", 0),
-			EphemeralConst.of(() -> random().nextDouble(10)));
-
-	private static final List<Sample<Double>> SAMPLES = Sample.parseDoubles("""
-			-1.0, -8.0000
-			-0.9, -6.2460
-			-0.8, -4.7680
-			-0.7, -3.5420
-			-0.6, -2.5440
-			-0.5, -1.7500
-			-0.4, -1.1360
-			-0.3, -0.6780
-			-0.2, -0.3520
-			-0.1, -0.1340
-			 0.0,  0.0000
-			 0.1,  0.0740
-			 0.2,  0.1120
-			 0.3,  0.1380
-			 0.4,  0.1760
-			 0.5,  0.2500
-			 0.6,  0.3840
-			 0.7,  0.6020
-			 0.8,  0.9280
-			 0.9,  1.3860
-			 1.0,  2.0000
-			""");
-
-	private static final Regression<Double> REGRESSION = Regression
-			.of(Regression.codecOf(OPS, TMS, 5, t -> t.gene().size() < 30), Error.of(LossFunction::mse), SAMPLES);
-
-	private static final GraphMap GRID = (popSize, gridSize) -> {
-		HashMap<Integer, List<Integer>> map = new HashMap<>();
-		for (int i = 0; i < popSize; i++) {
-			ArrayList<Integer> neighbors = new ArrayList<>();
-			neighbors.add((i + 1) % popSize);
-			neighbors.add(i - 1 < 0 ? popSize - 1 : i - 1);
-			neighbors.add((i + gridSize) % popSize);
-			neighbors.add(i - gridSize < 0 ? popSize - gridSize + i : i - gridSize);
-			map.put(i, neighbors);
-		}
-		return map;
-	};
+	private static final ISeq<Op<Double>> OPS = ISeq.of(MathOp.ADD, MathOp.SUB, MathOp.MUL, MathOp.DIV, MathOp.SQRT,
+			MathOp.SIN, MathOp.COS);
 
 	public static void main(String... args) {
 
-		Map<Integer, List<Integer>> connections = GRID.getConnections(100, 10);
+		try {
 
-//		TreeFormatter formatter = TreeFormatter.TREE;
+			final Regression<Double> regression = generateProblem("feynman_I_10_7.tsv.gz");
 
-//		Genotype<ProgramGene<Double>> newInstance = REGRESSION.codec().encoding().newInstance();
-
-//		@SuppressWarnings("unchecked")
-//		EphemeralConst<Double> newInstance = EphemeralConst
-//				.of((Supplier<Double> & Serializable) () -> random().nextDouble(10));
-
-//		System.out.println(formatter.format(newInstance.gene()));
+			String output = evolve(regression, GraphMaps.barabasiAlbert(100, 5, 3));
+//			final EvolutionResult<ProgramGene<Double>, Double> er = engine.stream().limit(50).peek(statistics)
+//					.collect(EvolutionResult.toBestEvolutionResult());
 //
-//		System.out.println(REGRESSION.fitness(newInstance));
+//			final ProgramGene<Double> gene = er.bestPhenotype().genotype().gene();
+//
+//			TreeNode<Op<Double>> tree = gene.toTreeNode();
+//
+//			MathExpr.rewrite(tree);
 
-//		try {
-//			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//			ObjectOutputStream oos = new ObjectOutputStream(baos);
-//			oos.writeObject(newInstance);
-//
-//			ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()));
-//
-//			@SuppressWarnings("unchecked")
-//			Genotype<ProgramGene<Double>> o = (Genotype<ProgramGene<Double>>) ois.readObject();
-//
-//			System.out.println(formatter.format(o.gene()));
-//
-//			System.out.println(REGRESSION.fitness(o));
-//		} catch (IOException | ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+			writeOutput(output, // statistics.tsoString(),
+					"feynman_I_10_7.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-		final CellularEngine cellularEngine = new CellularEngine(connections::get, REGRESSION);
-		final EvolutionStatistics<Double, DoubleMomentStatistics> statistics = EvolutionStatistics.ofNumber();
-
+	private static String evolve(Regression<Double> problem, GraphMap connections) {
+		CellularEngine engine = new CellularEngine(connections, problem);
+		EvolutionStatistics<Double, MinMax<Double>> statistics = EvolutionStatistics.ofComparable();
 		EvolutionResult<ProgramGene<Double>, Double> er = EvolutionStream
-				.ofEvolution(() -> cellularEngine.start(100, 1), cellularEngine::evolve)
-				.limit(Limits.byFitnessThreshold(0.1)).peek(statistics)
-				.collect(EvolutionResult.toBestEvolutionResult());
+				.ofEvolution(() -> engine.start(connections.getConnections().size(), 1), engine::evolve).limit(10)
+				.peek(statistics).collect(EvolutionResult.toBestEvolutionResult());
 
-//		final Engine<ProgramGene<Double>, Double> engine = Engine.builder(REGRESSION).minimizing()
-//				.alterers(new SingleNodeCrossover<>(0.1), new Mutator<>()).build();
-//
-//		final EvolutionResult<ProgramGene<Double>, Double> er = engine.stream().limit(Limits.byFitnessThreshold(0.01))
-//				.limit(100).collect(EvolutionResult.toBestEvolutionResult());
-
-		final ProgramGene<Double> gene = er.bestPhenotype().genotype().gene();
-
-		TreeNode<Op<Double>> tree = gene.toTreeNode();
+		TreeNode<Op<Double>> tree = er.bestPhenotype().genotype().gene().toTreeNode();
 
 		MathExpr.rewrite(tree);
 
-		System.out.println("G" + er.totalGenerations());
-		System.out.println("F" + new MathExpr(tree));
-		System.out.println("V" + gene.isValid());
-		System.out.println("E" + REGRESSION.error(tree));
+		return String.format("%s%n%n%s", new MathExpr(tree).toString(), statistics.toString());
+	}
 
-		System.out.println(statistics);
+	private static Regression<Double> generateProblem(String fileName) throws IOException {
+		String filepath = Objects.requireNonNull(Main.class.getClassLoader().getResource(fileName)).getFile();
+
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(new GZIPInputStream(new FileInputStream(filepath))))) {
+			String header = br.readLine();
+
+			String[] vars = header.split("\t");
+
+			ArrayList<Op<Double>> terminals = new ArrayList<>(vars.length);
+			for (int i = 0; i < vars.length - 1; i++) {
+				terminals.add(Var.of(vars[i], i));
+			}
+			terminals.add(EphemeralConst.of(() -> random().nextDouble(10)));
+
+			final String csvData = br.readAllAsString().replace("\t", ", ");
+			List<Sample<Double>> samples = Sample.parseDoubles(csvData);
+			Codec<Tree<Op<Double>, ?>, ProgramGene<Double>> codec = Regression.codecOf(OPS, ISeq.of(terminals), 5,
+					gt -> gt.gene().size() < 50);
+			return Regression.of(codec, Error.of(LossFunction::mse), samples);
+		}
+	}
+
+	private static void writeOutput(String output, String outFilename) {
+		try {
+			File dataDir = new File("data");
+
+			if (!dataDir.exists() && !dataDir.mkdir()) {
+				throw new IOException("Impossible to craete 'data' foler");
+			}
+
+			File outFile = new File(dataDir, outFilename);
+			if (!outFile.exists() && !outFile.createNewFile()) {
+				throw new IOException(String.format("Impossible to create '%s'", outFilename));
+			}
+			try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile)))) {
+				bw.write(output);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
